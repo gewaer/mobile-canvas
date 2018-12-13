af11c2ca2edd092dcdd7cae7fb55ee16db6acd16
Enmanuel <enmanuel@mctekk.com>
2018-12-13 16:53:58 -0400

    Add reports screen

---
+++ app/screens/reports/stylesheet.js
@@ -0,0 +1,81 @@
+import { StyleSheet, Platform, Dimensions } from "react-native";
+import {
+    colors,
+    paddingHelpers,
+} from "../../config/styles";
+// Responsive Const
+const deviceHeight = Dimensions.get("window").height;
+const deviceWidth = Dimensions.get("window").width;
+const platform = Platform.OS;
+const platformStyle = undefined;
+const isIphoneX =
+  platform === "ios" && deviceHeight === 812 && deviceWidth === 375;
+export default StyleSheet.create({
+    linkBTN: {
+        color: colors.brandOrange,
+        textDecorationLine: "underline",
+        fontSize: 14
+    },
+    submitBtn: {
+        backgroundColor: colors.brandRed,
+        shadowColor: '#000',
+        shadowOffset: { width: 0, height: 3 },
+        shadowOpacity: 0.2,
+        shadowRadius: 4,
+        width: 280,
+        alignSelf: 'center',
+        height: 36
+    },
+    googleBtn: {
+        backgroundColor: colors.gmail,
+        width: '100%',
+        alignItems: 'center'
+    },
+    googleText: {
+        color: "#fff",
+        textAlign: 'center',
+        width: '100%'
+    },
+    facebookBtn: {
+        marginTop: paddingHelpers.S,
+        marginBottom: paddingHelpers.XS,
+        backgroundColor: colors.facebook,
+        width: '100%',
+        alignItems: 'center'
+    },
+    facebookText: {
+        color: "#fff",
+        textAlign: 'center',
+        width: '100%'
+    },
+    formItem: {
+        borderBottomWidth: 2,
+        borderBottomColor: '#241D1E'
+    },
+    titleBarContent: {
+        color: "white",
+        fontWeight: "600"
+    },
+    containerView: {
+        marginBottom: paddingHelpers.XS
+    },
+    containerViewBack: {
+        flexGrow: 1,
+        marginTop: paddingHelpers.XS,
+        marginBottom: paddingHelpers.XS,
+        paddingVertical: paddingHelpers.N, 
+        backgroundColor: colors.brandWhite,
+        alignItems: 'center',
+        paddingHorizontal: paddingHelpers.N,
+    },
+    textContainer: {
+        alignItems: "center",
+        marginTop: 10,
+        backgroundColor: "transparent"
+    },
+    divisionLine: {
+        height: 10,
+        borderBottomWidth: 1,
+        borderBottomColor: colors.brandOrange
+    }
+});
