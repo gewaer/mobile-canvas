9b8175056e06670bd3f75a3064d2bdd185cdb84a
Enmanuel <enmanuel@mctekk.com>
2018-12-18 16:48:54 -0400

    Add content type header to axios interceptor

--- app/config/axios.js
+++ app/config/axios.js
@@ -11,6 +11,7 @@ instance.interceptors.request.use(function (config) {
     if(!config.url.includes('auth')){
         const token = getStore().getState().session.token;
         config.headers.Authorization = token;
+        config.headers.Accept = "application/json, text/plain, */*";
     }
     return config;
   }, function (error) {
