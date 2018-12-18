ce9f9b2b11e3bcf88ae579e3f9f86b770a8921ca
Enmanuel <enmanuel@mctekk.com>
2018-12-18 16:32:04 -0400

    Add axios interceptor

---
+++ app/config/axios.js
@@ -0,0 +1,20 @@
+import axios from "axios";
+import { VIVOOK_APP_BASE_API_URL } from "./env";
+import { Alert } from "react-native";
+import getStore from "../store/getStore";
+
+let instance = axios.create({
+    baseURL: VIVOOK_APP_BASE_API_URL
+})
+
+instance.interceptors.request.use(function (config) {
+    if(!config.url.includes('auth')){
+        const token = getStore().getState().session.token;
+        config.headers.Authorization = token;
+    }
+    return config;
+  }, function (error) {
+    return Promise.reject(error);
+  });
+
+module.exports = instance;
