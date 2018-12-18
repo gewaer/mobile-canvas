612a6cd0a26aad4e0863d03f61b2ea6b68fa41ad
Enmanuel <enmanuel@mctekk.com>
2018-12-18 16:29:28 -0400

    Add vivook base api url

--- app/config/env.js
+++ app/config/env.js
@@ -2,3 +2,4 @@ export const ENV = 'DEVELOPMENT';
 
 export const VUE_APP_BASE_API_URL = 'https://apicanvas.gewaer.io/v1';
 export const FORGOT_PASSWORD_URL = 'http://canvas.gewaer.io/users/forgot-password';
+export const VIVOOK_APP_BASE_API_URL = 'https://apidevvivook.mctekk.com/v2';
