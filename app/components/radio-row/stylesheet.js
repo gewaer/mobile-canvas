5fb2e5dc3da3491d708cc1e6ee892be13a342ede
Enmanuel <enmanuel@mctekk.com>
2018-12-19 15:14:40 -0400

    Update radio row component

--- app/components/radio-row/stylesheet.js
+++ app/components/radio-row/stylesheet.js
@@ -8,91 +8,13 @@ const platformStyle = undefined;
 const isIphoneX =
   platform === "ios" && deviceHeight === 812 && deviceWidth === 375;
 export default StyleSheet.create({
-  container: {
-    flexDirection: 'row'
-  },
-  drawerCover: {
-    alignSelf: "stretch",
-    height: deviceHeight / 3.5,
-    width: null,
-    position: "relative",
-    marginBottom: 10
-  },
-  drawerImage: {
-    position: "absolute",
-    left: Platform.OS === "android" ? deviceWidth / 10 : deviceWidth / 9,
-    top: Platform.OS === "android" ? deviceHeight / 13 : deviceHeight / 12,
-    width: 210,
-    height: 75,
-    resizeMode: "cover"
-  },
-  text: {
-    fontWeight: Platform.OS === "ios" ? "500" : "400",
-    fontSize: 16,
-    marginLeft: 20
-  },
-  badgeText: {
-    fontSize: Platform.OS === "ios" ? 13 : 11,
-    fontWeight: "400",
-    textAlign: "center",
-    marginTop: Platform.OS === "android" ? -3 : undefined
-  },
-  navItemStyle: {
-    padding: 10,
-    color: colors.brandBlack,
-    // fontFamily: "Akkurat-Bold",
-  },
-  navSectionStyle: {
-    backgroundColor: colors.brandWhite
-  },
-  sectionHeadingStyle: {
-    paddingVertical: 10,
-    paddingHorizontal: 5
-  },
-  footerContainer: {
-    padding: 20,
-    alignItems: "center",
-    backgroundColor: colors.brandDarkGrey
-  },
-  // header Style
-  userImage: {
-    width: 50,
-    height: 50,
-    marginHorizontal: 15
-  },
-  foregroundContainer: {
-    width: 300,
-    height: 140
-  },
-  logoutButton: {
-    position: "absolute",
-    top: 60,
-    right: 15
-  },
-  overlay: {
-    position: "absolute",
-    top: 0,
-    right: 0,
-    bottom: 0,
-    left: 0,
-    backgroundColor: "#000",
-    opacity: 0.6
-  },
-  userInfoContainer: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center"
-  },
-  // For bg colors
-  darkerColor: {
-    backgroundColor: "#1E2A39"
-  },
   thumbnail: {
     width: 40,
     height: 40,
     borderColor: colors.brandLightGray,
     borderWidth: 1,
-    borderRadius: 20
+    borderRadius: 20,
+    marginRight: 12
   },
   itemContainer: {
     borderBottomWidth: 0.6,
@@ -108,5 +30,9 @@ export default StyleSheet.create({
   },
   subTitle: {
     fontSize: 12
-  } 
+  },
+  rightStyle: {
+    alignSelf: 'center',
+    marginLeft: 'auto'
+  }
 });
