c9060506397352d14aeb9c87741aaa525ccf20cf
Enmanuel <enmanuel@mctekk.com>
2019-01-02 18:02:32 -0400

    Update comment row

--- app/components/comment-row/component.js
+++ app/components/comment-row/component.js
@@ -27,6 +27,7 @@ import {
 
 import Stylesheet from "./stylesheet";
 import ItemCount from "../item-count";
+import { dateFormat } from "../../lib/helpers";
 
 class PostRow extends Component {
 
@@ -34,13 +35,12 @@ class PostRow extends Component {
 		source: null,
 		group: 'ITERA - MEXICO',
 		user: 'Maria Esther Ramos',
-		content: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna...'
 	} 
 	
 	render() {
 		const {
 			title,
-			content,
+			comment,
 			subTitle,
 			isSelected,
 			onPress,
@@ -51,18 +51,19 @@ class PostRow extends Component {
 		} = this.props;
 
 		return (
-			<TouchableOpacity style={ Stylesheet.itemContainer } onPress={ this.props.onPress }>
+			<TouchableOpacity style={ Stylesheet.itemContainer } onPress={ onPress }>
 				<View style={ Stylesheet.topContainer }>
-					{ this.props.source ?
+					{ source ?
 					<Image
-						source={ { uri: this.props.source } }
+						source={ { uri: source } }
 						style={ Stylesheet.thumbnail }
 					/> :
 						<Icon type={'Ionicons'} name={'ios-contact'} style={ { color: '#B5B5B5',  fontSize: 32, marginRight: 12, marginTop: -5, marginBottom: -10} } />
 					}
 					<View style={ { maxWidth: '85%' } }>
-						<Text style={ Stylesheet.title }>{ this.props.group }, { this.props.user } </Text>
-						<Text style={ Stylesheet.content }>{ this.props.content }</Text>
+						<Text style={ Stylesheet.title }>{ this.props.user } - { this.props.group }</Text>
+						<Text style={ [{ fontSize: 12, color: colors.brandLightGray }] }>{ dateFormat(comment.CmmtCreatedDate) }</Text>
+						<Text style={ Stylesheet.content }>{ comment.CmmtText }</Text>
 					</View>
 				</View>
 			</TouchableOpacity>
