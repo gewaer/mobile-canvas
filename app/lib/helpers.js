154393f647ee9abc7d57931337e20681a57259b9
Enmanuel <enmanuel@mctekk.com>
2019-01-02 18:02:57 -0400

    Update moment helper

--- app/lib/helpers.js
+++ app/lib/helpers.js
@@ -1,5 +1,6 @@
 import moment from "moment"
+import localization from 'moment/locale/es';
 
 export function dateFormat(date){
-    return moment(date).format("DD MMM[.] YYYY [a las] hh:mm A")
+    return moment(date).locale("es", localization).format("DD MMM YYYY [a las] hh:mm A")
 }
