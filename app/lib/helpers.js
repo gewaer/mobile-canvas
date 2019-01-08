52c9a53e2bd2310bf4c582e4b8878db04d8c7284
Enmanuel <enmanuel@mctekk.com>
2019-01-08 17:11:49 -0400

    Add helpers

--- app/lib/helpers.js
+++ app/lib/helpers.js
@@ -1,6 +1,15 @@
 import moment from "moment"
 import localization from 'moment/locale/es';
 
-export function dateFormat(date){
+export function dateHourFormat(date){
     return moment(date).locale("es", localization).format("DD MMM YYYY [a las] hh:mm A")
+}
+
+export function dateFormat(date){
+    return moment(date).locale("es", localization).format("DD MMM YYYY")
+}
+export function amountFormat(amount){
+    let intAmount = amount.replace(',', '');
+    intAmount = intAmount.replace('$', '');
+    return intAmount*1;
 }
