dee14f49b9b543adf65e495f089f7428aed0290c
Enmanuel <enmanuel@mctekk.com>
2019-01-08 17:13:42 -0400

    Update bills screen

--- app/screens/bills/stylesheet.js
+++ app/screens/bills/stylesheet.js
@@ -11,22 +11,6 @@ const platformStyle = undefined;
 const isIphoneX =
   platform === "ios" && deviceHeight === 812 && deviceWidth === 375;
 export default StyleSheet.create({
-    linkBTN: {
-        color: '#00A1B4',
-        textDecorationLine: "underline",
-        fontSize: 14
-    },
-    submitBtn: {
-        backgroundColor: '#00A1B4',
-        shadowColor: '#000',
-        shadowOffset: { width: 0, height: 3 },
-        shadowOpacity: 0.2,
-        shadowRadius: 4,
-        width: 169,
-        alignSelf: 'flex-end',
-        height: 30,
-        marginHorizontal: 16
-    },
     formItem: {
         borderTopWidth: 1,
         borderLeftWidth: 1,
@@ -36,14 +20,6 @@ export default StyleSheet.create({
         height: 29.5,
         borderRadius: 3
     },
-    formInput: {
-        borderWidth: 1,
-        borderColor: '#00A1B4',
-        marginTop: 8,
-        fontSize: 12,
-        paddingLeft: 16,
-        paddingRight: 16
-    },
     titleBarContent: {
         color: "white",
         fontWeight: "600"
@@ -51,20 +27,6 @@ export default StyleSheet.create({
     containerView: {
         marginBottom: paddingHelpers.S,
         marginHorizontal: 12
-    },
-    containerViewBack: {
-        flexGrow: 1,
-        marginTop: paddingHelpers.XS,
-        marginBottom: paddingHelpers.XS,
-        paddingVertical: paddingHelpers.N, 
-        backgroundColor: colors.brandWhite,
-        alignItems: 'center',
-        paddingHorizontal: paddingHelpers.N,
-    },
-    textContainer: {
-        alignItems: "center",
-        marginTop: 10,
-        backgroundColor: "transparent"
     },
     divisionLine: {
         height: 1,
@@ -75,16 +37,25 @@ export default StyleSheet.create({
         fontSize: 14,
         marginTop: 10
     },
-    titleText: {
-        fontSize: 16,
-        color: '#00A1B4',
-        marginLeft: 12,
-        marginVertical: 12
+    expandedSection: {
+        borderWidth: 1,
+        borderTopWidth: 0,
+        paddingBottom: 5,
+        marginBottom: 5,
+        marginHorizontal: 12,
+        borderRadius: 3
     },
-    buttonIcon: {
-        marginLeft: 0,
-        marginRight: 0,
-        paddingTop: 0,
-        height: 25,
+    balanceRow:{
+        alignItems: 'center',
+        justifyContent: 'center',
+        height: 75
+    },
+    balanceTitle: {
+        fontSize: 16,
+        color: '#241D1E',
+        marginBottom: 5
+    },
+    balanceSubTitle: {
+        fontSize: 12
     }
 });
