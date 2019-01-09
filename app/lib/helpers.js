29ac67636f5f3999266065098689dd8fedd6c24a
Enmanuel <enmanuel@mctekk.com>
2019-01-09 10:14:04 -0400

    Update helper

--- app/lib/helpers.js
+++ app/lib/helpers.js
@@ -9,7 +9,5 @@ export function dateFormat(date){
     return moment(date).locale("es", localization).format("DD MMM YYYY")
 }
 export function amountFormat(amount){
-    let intAmount = amount.replace(',', '');
-    intAmount = intAmount.replace('$', '');
-    return intAmount*1;
+    return Number(amount.match(/\d|\./g).join(""));
 }
