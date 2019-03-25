8afd972f80930773afe53e89d0c9ca1938e98903
Enmanuel <enmanuel@mctekk.com>
2019-03-25 16:48:54 -0400

    Link document picker

--- android/app/src/main/java/com/canvas/MainApplication.java
+++ android/app/src/main/java/com/canvas/MainApplication.java
@@ -3,6 +3,7 @@ package com.canvas;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.reactnativedocumentpicker.ReactNativeDocumentPicker;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
