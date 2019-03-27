51a62b5800010aec352eec3582358211211dc1e4
Enmanuel <enmanuel@mctekk.com>
2019-03-27 10:48:28 -0400

    Android document picker

--- android/app/src/main/java/com/canvas/MainApplication.java
+++ android/app/src/main/java/com/canvas/MainApplication.java
@@ -39,6 +39,7 @@ public class MainApplication extends NavigationApplication {
       // No need to add RnnPackage and MainReactPackage
       return Arrays.<ReactPackage>asList(
           // eg. new VectorIconsPackage()
+          new ReactNativeDocumentPicker()
       );
   }
 
