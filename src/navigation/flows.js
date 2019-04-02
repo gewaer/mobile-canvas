55ce2f2f47d15cc860d54569c5d7f486d7ebc5f1
Enmanuel <enmanuel@mctekk.com>
2019-04-02 16:13:17 -0400

    Expo setup

--- src/navigation/flows.js
+++ src/navigation/flows.js
@@ -145,6 +145,29 @@ export function pushDashboard(sideMenuProps) {
                     }
                   }
                 }
+              },
+              {
+                component: {
+                  id: 'navigation.myScreen',
+                  name: 'canvas.MyScreen',
+                  options: {
+                    bottomTab: {
+                      iconColor: colors.brandSecondary,
+                      selectedIconColor: colors.brandPrimary,
+                      icon: require('../assets/images/icons/apps.png'),
+                      // add this to change icon position (optional, iOS only).
+                      iconInsets: {
+                        top: 6, // optional, default is 0.
+                        left: 0, // optional, default is 0.
+                        bottom: -6, // optional, default is 0.
+                        right: 0 // optional, default is 0.
+                      }
+                    },
+                    topBar: {
+                      visible: false
+                    }
+                  }
+                }
               }
             ]
           }
