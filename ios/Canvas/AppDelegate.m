9a61e6ef12924b2e5959e4cc6c2ffe4b18ea7f40
Sebastian Perez <sperezm97@gmail.com>
2019-05-27 16:14:46 -0400

    Fix installation

--- ios/Canvas/AppDelegate.m
+++ ios/Canvas/AppDelegate.m
@@ -9,6 +9,11 @@
 #import <React/RCTBridge.h>
 #import <React/RCTBundleURLProvider.h>
 #import <React/RCTRootView.h>
+#if __has_include(<React/RNSentry.h>)
+#import <React/RNSentry.h> // This is used for versions of react >= 0.40
+#else
+#import "RNSentry.h" // This is used for versions of react < 0.40
+#endif
 #import <React/RNSentry.h> // This is used for versions of react >= 0.40
 #import <ReactNativeNavigation/ReactNativeNavigation.h>
 #import <FBSDKCoreKit/FBSDKCoreKit.h>
